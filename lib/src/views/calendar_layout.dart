483967929450b1f26583366d8d4fe0ac39f7cefe
threeyeies <diamcrustcoding@gmail.com>
2024-10-16 20:59:25 -0600

    designing the calendar layout
    debug banner is desabled

--- lib/src/views/calendar_layout.dart
+++ lib/src/views/calendar_layout.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarLayout extends StatelessWidget {
   const CalendarLayout({super.key});
@@ -7,12 +6,35 @@ class CalendarLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-          child: SfCalendar(
-            view: CalendarView.week,
-            firstDayOfWeek: 1,
-            
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Theme.of(context).colorScheme.primary,
+              child: Center(
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Item $index', style: Theme.of(context).textTheme.labelSmall),
+                    );
+                  },
+                ),
+              ),
+            ),
           ),
-
+          Expanded(
+            flex: 15,
+            child: Container(
+              color: Theme.of(context).colorScheme.secondary,
+              child: const Center(
+                child: Text('Zona del Evento'),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
